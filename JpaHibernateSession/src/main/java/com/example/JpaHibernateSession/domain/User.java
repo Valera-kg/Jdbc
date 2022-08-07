ddfcf63eef3846a93951d5ec0e6496f811eb4cc5
Alexander Ibragimov <iva-kg@yandex.ru>
2022-08-08 00:57:07 +0600

    Revert "Delete JpaHibernateSession directory"
    This reverts commit 7ce5c731

---
+++ JpaHibernateSession/src/main/java/com/example/JpaHibernateSession/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.JpaHibernateSession.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="persondb")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="person_id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    public User() {
+    }
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
